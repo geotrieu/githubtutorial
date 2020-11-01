a96892a47ff5f0a3366811d4ce21bfe0ee4cd5c8
geotrieu <trieugeorge@gmail.com>
2020-11-01 16:39:40 -0500

    test

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 # Just an ordinary test file
 
 print("Deprecation"); # For the branch example, just replace my name with yours and save the file
-print("Alexia"); # For the branch example, just replace my name with yours and save the file
